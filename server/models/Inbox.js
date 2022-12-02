bdd03803c15103c1ebe80c3355d4b327afe92498
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-12-02 16:42:24 +0000

    added inboxSchema

---
+++ server/models/Inbox.js
@@ -0,0 +1,15 @@
+const { Schema} = require("mongoose");
+
+const inboxSchema = new Schema({
+    inboxParticipants: [{
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }],
+    lastMessage: String,
+    lastSentUserId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+  });
+  
+  module.exports = inboxSchema;
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,17 +1,18 @@
 const { Schema, model } = require("mongoose");
-
+const inboxSchema = require ("./Inbox");
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const messageSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  inboxId: inboxSchema,
+  messageText: {type: String, 
+    required: true,
+  },
   read: { 
     type: Boolean, 
     default: false,
-  },
-  messageText: {type: String, 
-    required: true,
   },
   dateCreated: { 
     type: Date, 

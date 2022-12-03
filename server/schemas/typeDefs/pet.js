275953aafe97f828df703215681d23341b5f59de
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-12-03 01:10:13 +0000

    moved pet typedefs to pet.js

---
+++ server/schemas/typeDefs/pet.js
@@ -0,0 +1,55 @@
+const { gql } = require("apollo-server-express");
+export default gql`
+  type Pet {
+    _id: ID
+    name: String!
+    age: String!
+    gender: String!
+    species: String!
+    description: String!
+    city: String!
+    country: String!
+    breed: String
+    medicalHistory: String!
+    colour: String
+    image: String
+    dateCreated: String
+    userlikeCount: Int
+    commentCount: Int
+    comments: [Comment]
+  }
+  type Comment {
+    _id: ID
+    userId: User
+    commentBody: String!
+    dateCreated: String
+  }
+  input PetInput {
+    _id: ID
+    name: String!
+    age: String!
+    gender: String!
+    species: String!
+    description: String!
+    city: String!
+    country: String!
+    breed: String
+    medicalHistory: String!
+    colour: String
+    image: String
+    dateCreated: String
+  }
+  type Query {
+    pets: [Pet]!
+    pet(petId: ID!): Pet
+  }
+  type Mutation {
+    savePet(pet: PetInput!): User
+    addPet(input: PetInput!): User
+    updatePet(petId: ID!, input: PetInput!): Pet
+    removePet(petId: ID!): Pet
+    removeLikedPet(petId: ID!): User
+    addComment(petId: ID!, commentBody: String!): Pet
+    removeComment(petId: ID!, commentId: ID!): Pet
+  }
+`
--- server/schemas/typeDefs/user.js
+++ server/schemas/typeDefs/user.js
@@ -1,5 +1,5 @@
 const { gql } = require("apollo-server-express");
-const typeDefs = gql`
+export default gql`
   type User {
     _id: ID!
     fullname: String!

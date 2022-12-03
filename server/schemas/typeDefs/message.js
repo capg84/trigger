8f8888a311fa880072d1595b5ea065b5e402c55b
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-12-03 01:12:21 +0000

    moved message typedefs to message.js

---
+++ server/schemas/typeDefs/message.js
@@ -0,0 +1,21 @@
+const { gql } = require("apollo-server-express");
+export default gql`
+  type Message {
+    _id: ID!
+    messageText: String!
+    read: Boolean
+    dateCreated: String
+    from: User!
+    to: User!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    messages(from: String!): [Message]!
+  }
+  type Mutation {
+    sendMessage(to: ID!, messageText: String!): Message
+  }
+`

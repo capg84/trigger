62e6c4e8855134eda6c311b8b249f77d880eada2
P Luong <p-luong@live.co.uk>
2022-12-04 16:37:22 +0000

    imported css to dashboard files and created a dashboard container

--- client/src/Components/dashboard-nav.js
+++ client/src/Components/dashboard-nav.js
@@ -1,12 +1,14 @@
+import '../Assets/Styles/dashboard.css';
+
 const DashboardNav = ({ currentComponent, handleComponentChange }) => {
     return (
         <nav className="dashboard-nav">
             <div className="links">
-                <a className="item-link" href="#Create" onClick={() => handleComponentChange('Create')}>CREATE A LISTING</a>
-                <a className="item-link" href="#Manage" onClick={() => handleComponentChange('Manage')}>MANAGE A LISTING</a>
-                <a className="item-link" href="#Favourites" onClick={() => handleComponentChange('Favourites')}>♡ FAVOURITES</a>
-                <a className="item-link" href="#Messages" onClick={() => handleComponentChange('Messages')}>MY MESSAGES</a>
-                <a className="item-link" href="#Account" onClick={() => handleComponentChange('Account')}>UPDATE ACCOUNT DETAILS</a>
+                <a className="item-link-nav" href="#Create" onClick={() => handleComponentChange('Create')}>CREATE A LISTING</a>
+                <a className="item-link-nav" href="#Manage" onClick={() => handleComponentChange('Manage')}>MANAGE A LISTING</a>
+                <a className="item-link-nav" href="#Favourites" onClick={() => handleComponentChange('Favourites')}>♡ FAVOURITES</a>
+                <a className="item-link-nav" href="#Messages" onClick={() => handleComponentChange('Messages')}>MY MESSAGES</a>
+                <a className="item-link-nav" href="#Account" onClick={() => handleComponentChange('Account')}>UPDATE ACCOUNT DETAILS</a>
             </div>
         </nav>
     );

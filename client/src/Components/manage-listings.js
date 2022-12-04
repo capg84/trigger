cf0e771431b8f72026bf0af9b8f9e54c646ea532
P Luong <p-luong@live.co.uk>
2022-12-04 17:38:31 +0000

    updated styling for article and added another card

--- client/src/Components/manage-listings.js
+++ client/src/Components/manage-listings.js
@@ -1,20 +1,48 @@
 const ManageListing = () => {
   return (
-    <div className="listings-container">
-      <article className="listing-item">
-        <div className="listing-image">
+    <div className="outer-container">
+      <div className="listings-container">
+        <article className="listing-item">
+          <div className="listing-image">
             <p>pet image here</p>
+          </div>
+          <div className="listing-info">
+            <p>Pet name</p>
+            <p>Location</p>
+            <p>Age</p>
+            <p>Gender</p>
+          </div>
+        </article>
+        <div className="listing-buttons">
+          <button className="edit-btn" id="edit">
+            EDIT
+          </button>
+          <button className="del-btn" id="delete">
+            DELETE
+          </button>
         </div>
-        <div className="listing-info">
-          <p>Pet name</p>
-          <p>Location</p>
-          <p>Age</p>
-          <p>Gender</p>
+      </div>
+
+      <div className="listings-container">
+        <article className="listing-item">
+          <div className="listing-image">
+            <p>pet image here</p>
+          </div>
+          <div className="listing-info">
+            <p>Pet name</p>
+            <p>Location</p>
+            <p>Age</p>
+            <p>Gender</p>
+          </div>
+        </article>
+        <div className="listing-buttons">
+          <button className="edit-btn" id="edit">
+            EDIT
+          </button>
+          <button className="del-btn" id="delete">
+            DELETE
+          </button>
         </div>
-      </article>
-      <div className="listing-buttons">
-          <button className="edit-btn" id="edit">EDIT</button>
-          <button className="del-btn" id="delete">DELETE</button>
       </div>
     </div>
   );

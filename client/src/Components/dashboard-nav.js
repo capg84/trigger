ae9dc2a6e9af31f9ce93c0af8cd736fd3208163c
P Luong <p-luong@live.co.uk>
2022-12-04 15:53:48 +0000

    added links for dashboard navbar

--- client/src/Components/dashboard-nav.js
+++ client/src/Components/dashboard-nav.js
@@ -1,8 +1,14 @@
-const DashboardNav = () => {
+const DashboardNav = ({ currentComponent, handleComponentChange }) => {
     return (  
-        <div>
-            <h1>Dashboard Nav here</h1>
-        </div>
+        <nav className="dashboard-nav">
+            <div className="links">
+                <a className="item-link" href="#Home" onClick={() => handleComponentChange('Create')}>CREATE A LISTING</a>
+                <a className="item-link" href="#Portfolio" onClick={() => handleComponentChange('Manage')}>MANAGE A LISTING</a>
+                <a className="item-link" href="#Experience" onClick={() => handleComponentChange('Favourites')}>♡ FAVOURITES</a>
+                <a className="item-link" href="#Contact" onClick={() => handleComponentChange('Messages')}>MY MESSAGES</a>
+                <a className="item-link" href="#Contact" onClick={() => handleComponentChange('Account')}>UPDATE ACCOUNT DETAILS</a>
+            </div>
+        </nav>
     );
 }
  

cf8785f3650fd746b94e5ce0011c53850520137a
DMon <davemonaghan@gmx.com>
2022-12-04 17:48:37 +0000

    create page created

--- client/src/Components/create-pet.js
+++ client/src/Components/create-pet.js
@@ -1,112 +1,87 @@
-import FileBase64 from 'react-file-base64';
+// import FileBase64 from 'react-file-base64';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useState, useMutation } from 'react'
+// import { useState, useMutation } from 'react'
 
 
 
-function createPet() {
+function CreatePet() {
 
-//   const [ pet, usePet ] = useState({
-//     species: "",
-//     breed: "",
-//     colour: "",
-//     name: "",
-//     image: {
-//       title: "",
-//       image: ""
-//     },
-//     description: "",
-//     city: "",
-//     country: "",
-//     medicalHistory: "",
-
-// })
 
  
   return (
-    <Form>
-      <Form.Group className="mb-3" >
-        <Form.Label>Animal species</Form.Label>
-        <Form.Control type="species" placeholder="Enter species" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+    <Form style={{backgroundColor: "#AD7940", opacity: "0.8", width: "80%", marginLeft: "10%", marginTop: "100px",
+    marginBottom: "150px", padding: "2vh" }}>
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>ANIMAL SPECIES:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }} type="species" placeholder="REQUIRED" />
       </Form.Group>
 
-       <Form.Group className="mb-3" >
-        <Form.Label>Breed</Form.Label>
-        <Form.Control type="breed" placeholder="Breed" />
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>BREED:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }}  type="species" placeholder="REQUIRED" />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
-        <Form.Label>Colour</Form.Label>
-        <Form.Control type="colour" placeholder="Colour" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }}className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>COLOUR:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }}  type="colour" placeholder="REQUIRED" />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="name" placeholder="Name" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>NAME:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }}  type="name" placeholder="REQUIRED" />
       </Form.Group>
 
-      {/* <Form.Group className="mb-3" >
-        <Form.Label>Upload image of your pet</Form.Label>
-        <Form.Control type="file" placeholder="Image" accept="image/png image.jpg"
-        onchange={e => setItem({...item, title: e.taret.value })}/>
-        <FileBase64
-        multiple={ false }
-        onDone={ ({base64}) => setItem({...item, image: base64 }) } />
-        <Button variant="primary" type="btn">
-        Choose image
-      </Button>
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
-      </Form.Group> */}
+      <Form.Group style={{ width: "100%", marginLeft: "3%" ,  }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px"  }}>UPLOAD IMAGE OF YOUR PET:</Form.Label>
+        <Form.Control style={{ width: "81%", fontSize: "20px", display: "inline-block" }} type="file" placeholder="ENTER IMAGE" accept="image/png image.jpg" />
+        <Button style={{   backgroundColor: "#9CCBC3", color: "#f2faf5", fontSize: "20px", marginLeft:"1vh", marginBottom:"1vh"}}
+        variant="primary" type="btn">UPLOAD IMAGE</Button>
+      </Form.Group> 
 
-      <Form.Group className="mb-3" >
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="description" placeholder="Description" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "94%", marginLeft: "3%"  }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "95%", fontSize: "20px" }}>DESCRIPTION:</Form.Label>
+        <textarea style={{color: "#AD7940", fontSize: "20px", height:"100px", width: "99%"}}  type="description" placeholder="REQUIRED" 
+        className="form-control input" />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
-        <Form.Label>City</Form.Label>
-        <Form.Control type="city" placeholder="City" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>CITY:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }}  type="city" placeholder="REQUIRED" />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
-        <Form.Label>Country</Form.Label>
-        <Form.Control type="country" placeholder="Country" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "45%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>COUNRTY:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }}  type="country" placeholder="REQUIRED" />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
-        <Form.Label>Medical history</Form.Label>
-        <Form.Control type="medical-history" placeholder="Medical history" />
-        <Form.Text className="text-muted">
-          required
-        </Form.Text>
+      <Form.Group style={{ width: "93%", marginLeft: "3%"  }} className="mb-3" >
+        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "20px" }}>MEDICAL HISTORY:</Form.Label>
+        <Form.Control style={{color: "#AD7940", fontSize: "20px" }} type="medical-history" placeholder="REQUIRED" />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Create Pet
+      <Button style={{  width: "25vh", marginLeft: "42%", backgroundColor: "#9CCBC3", color: "#f2faf5", marginBottom: "2vh", marginTop: "2vh", fontSize: "20px"}}
+       variant="primary" type="submit">
+        CREATE PET
       </Button>
     </Form>
   );
 }
 
-export default createPet;
+export default CreatePet;
+
+
+// <Form.Group className="mb-3" >
+// <Form.Label>Upload image of your pet</Form.Label>
+// <Form.Control type="file" placeholder="Image" accept="image/png image.jpg" />
+// // onchange={e => setItem({...item, title: e.taret.value })}/>
+// // <FileBase64
+// // multiple={ false }
+// // onDone={ ({base64}) => setItem({...item, image: base64 }) } />
+// <Button variant="primary" type="btn">
+// Choose image
+// </Button>
+// <Form.Text className="text-muted">
+//   required
+// </Form.Text>
+// </Form.Group> 

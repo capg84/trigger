147550a17bd6cbc1c732febd75cb95b3f1398fc0
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-04 15:54:12 +0000

    modified queries.js, added MY_PROFILE query and modified USER_PROFILES

--- client/src/Utils/queries.js
+++ client/src/Utils/queries.js
@@ -89,36 +89,107 @@ export const GET_MESSAGES = gql`
 
 `;
 
-export const USER_PROFILE = gql`{
-    user {
-      fullname
-      email
-      city
-      country
-      description
-      userpets {
-        _id
-        species
-        name
-        age
-        gender
-        description
+export const USER_PROFILES = gql`{
+    users {
+        fullname
+        email
         city
         country
-        breed
-        medicalHistory
-        colour
-        image
-        dateCreated
-        } 
-    Comments {
-        _id
-        userId
-        commentBody
-        dateCreated
-    }
-        
-      }
+        description
+        userpets {
+            _id
+            species
+            name
+            age
+            gender
+            description
+            city
+            country
+            breed
+            medicalHistory
+            colour
+            image
+            dateCreated
+            } 
+        LikedPets{
+            _id
+            species
+            name
+            age
+            gender
+            description
+            city
+            country
+            breed
+            medicalHistory
+            colour
+            image
+            dateCreated
+        }
+        messages {
+            _id
+            messageText
+            read
+            dateCreated
+            from {
+                fullname
+            }
+            to {
+                fullname
+            }
+        }
     }
 `;
 
+export const MY_PROFILE = gql`{
+    query singleUser ($_id: ID!){
+      me(_id: $_id){
+        fullname
+        email
+        city
+        country
+        description
+        userpets {
+            _id
+            species
+            name
+            age
+            gender
+            description
+            city
+            country
+            breed
+            medicalHistory
+            colour
+            image
+            dateCreated
+            } 
+        LikedPets{
+            _id
+            species
+            name
+            age
+            gender
+            description
+            city
+            country
+            breed
+            medicalHistory
+            colour
+            image
+            dateCreated
+        }
+        messages {
+            _id
+            messageText
+            read
+            dateCreated
+            from {
+                fullname
+            }
+            to {
+                fullname
+            }
+        }
+    }
+`;

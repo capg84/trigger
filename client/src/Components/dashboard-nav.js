864b6cce0266a09ddcde5b65b445f198e8d5d019
P Luong <p-luong@live.co.uk>
2022-12-04 15:55:19 +0000

    updated href for links

--- client/src/Components/dashboard-nav.js
+++ client/src/Components/dashboard-nav.js
@@ -2,11 +2,11 @@ const DashboardNav = ({ currentComponent, handleComponentChange }) => {
     return (  
         <nav className="dashboard-nav">
             <div className="links">
-                <a className="item-link" href="#Home" onClick={() => handleComponentChange('Create')}>CREATE A LISTING</a>
-                <a className="item-link" href="#Portfolio" onClick={() => handleComponentChange('Manage')}>MANAGE A LISTING</a>
-                <a className="item-link" href="#Experience" onClick={() => handleComponentChange('Favourites')}>♡ FAVOURITES</a>
-                <a className="item-link" href="#Contact" onClick={() => handleComponentChange('Messages')}>MY MESSAGES</a>
-                <a className="item-link" href="#Contact" onClick={() => handleComponentChange('Account')}>UPDATE ACCOUNT DETAILS</a>
+                <a className="item-link" href="#Create" onClick={() => handleComponentChange('Create')}>CREATE A LISTING</a>
+                <a className="item-link" href="#Manage" onClick={() => handleComponentChange('Manage')}>MANAGE A LISTING</a>
+                <a className="item-link" href="#Favourites" onClick={() => handleComponentChange('Favourites')}>♡ FAVOURITES</a>
+                <a className="item-link" href="#Messages" onClick={() => handleComponentChange('Messages')}>MY MESSAGES</a>
+                <a className="item-link" href="#Account" onClick={() => handleComponentChange('Account')}>UPDATE ACCOUNT DETAILS</a>
             </div>
         </nav>
     );

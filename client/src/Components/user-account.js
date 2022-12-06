8db63ec88bcc42fc9c3a8a72b90a8ce2f2fbc187
P Luong <p-luong@live.co.uk>
2022-12-06 13:45:58 +0000

    added state variables for user account form

--- client/src/Components/user-account.js
+++ client/src/Components/user-account.js
@@ -1,7 +1,36 @@
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useMutation } from '@apollo/client';
+import { UPDATE_USER } from '../Utils/mutations';
 
 const Account = () => {
+  // Create state variables for the fields in the form
+  const [fullname, setFullname] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [city, setCity] = useState();
+  const [country, setCountry] = useState();
+  const [description, setDescription] = useState();
+
+  const [aboutMe, { error }] = useMutation(UPDATE_USER);
+
+  const handleEditUser = async (event) => {
+    event.preventDefault();
+    console.log(event);
+      try {
+        const { data } = await aboutMe({
+          variables: { } 
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    if (!user.length) {
+      return <h3>No user found</h3>;
+    }
+
   return (
     <div className="formContainer">
 
@@ -10,32 +39,27 @@ const Account = () => {
     marginBottom: "150px", padding: "2vh" }}>
       <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
         <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>FULL NAME:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }} type="species" placeholder="EXISTING NAME" />
+        <Form.Control style={{color: "#AD7940", fontSize: "15px" }} type="text" placeholder="EXISTING NAME" />
       </Form.Group>
 
       <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
         <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>EMAIL:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="species" placeholder="EXISTING EMAIL" />
+        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="email" placeholder="EXISTING EMAIL" />
       </Form.Group>
 
       <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" controlId="formBasicPassword" >
         <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>PASSWORD:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="colour" placeholder="***********" />
+        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="password" placeholder="***********" />
       </Form.Group>
 
       <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
         <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>CITY:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="name" placeholder="EXISTING CITY" />
+        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="text" placeholder="EXISTING CITY" />
       </Form.Group>
 
       <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
         <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>COUNTRY:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="name" placeholder="EXISTING COUNTRY" />
-      </Form.Group>
-
-      <Form.Group style={{ width: "94%", marginLeft: "3%", display: "inline-block" }} className="mb-3" >
-        <Form.Label style={{color: "#f2faf5", width: "80%", fontSize: "15px" }}>LOOKING FOR:</Form.Label>
-        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="name" placeholder="Existing looking for information" />
+        <Form.Control style={{color: "#AD7940", fontSize: "15px" }}  type="text" placeholder="EXISTING COUNTRY" />
       </Form.Group>
 
       <Form.Group style={{ width: "94%", marginLeft: "3%"  }} className="mb-3" >

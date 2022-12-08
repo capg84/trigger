450d488c760cb75cba47e9ef3b5813109da71746
P Luong <p-luong@live.co.uk>
2022-12-08 19:05:30 +0000

    updated pet

--- client/src/Utils/mutations.js
+++ client/src/Utils/mutations.js
@@ -62,8 +62,8 @@ export const SAVE_PET = gql`
 `;
 
 export const CREATE_PET = gql`
-mutation addPet($breed: String, $colour: String, $name: String!, $age: String!, $gender: String!, $species: String!, $description: String!, $city: String!, $country: String!, $medicalHistory: String!, $image: String!) {
-  addPet(breed: $breed, colour: $colour, name: $name, age: $age, gender: $gender, species: $species, description: $description, city: $city, country: $country, medicalHistory: $medicalHistory, image: $image) {
+mutation addPet($breed: String, $colour: String, $name: String!, $age: String!, $gender: String!, $species: String!, $description: String!, $city: String!, $country: String!, $medicalHistory: String!, $image: String!, $owner: ID!) {
+  addPet(breed: $breed, colour: $colour, name: $name, age: $age, gender: $gender, species: $species, description: $description, city: $city, country: $country, medicalHistory: $medicalHistory, image: $image, owner: $owner) {
       _id
       name
       age

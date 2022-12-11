09725b6744dcda294855026c37946ee8d5db0d29
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 19:36:37 +0000

    corrected the revert

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if we're in production, serve client/build as static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
